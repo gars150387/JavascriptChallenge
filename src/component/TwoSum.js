2882fd550d7c46bc102bbab04271c6c7902377bc
Gustavo <gar.santeliz@gmail.com>
2021-11-20 17:00:42 -0500

    two sum leetcode solution

---
+++ src/component/TwoSum.js
@@ -0,0 +1,7 @@
+const twoSum -= (nums, target) =>{
+    let storage = {};
+    for (let [index, target] of nums.entries()){
+        if (storage[num] !== undefined) return [storage[num], index];
+        storage[target - num] = index;
+    }
+}

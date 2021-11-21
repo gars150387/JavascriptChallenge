ab660d3db78a26f5e62332119ec5829c0a829dbf
Gustavo <gar.santeliz@gmail.com>
2021-11-21 17:20:15 -0500

    max consecutive ones leetcode exercise

---
+++ src/component/MaxConsecutiveOnes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// [1,1,0,1,1,1] max consecutive of 1s is 3
+ var findMaxConsecutiveOnes = function(nums) {
+     let mc = 0;
+     let fm = 0;
+     
+     for (let i = 0; i < nums.length; i++){
+         if (nums[i] ===1){
+             cm++;
+             fm = Math.max(cm, fm)
+         }
+         else{
+             cm = 0;
+         }
+     }
+     return fm;
+ };

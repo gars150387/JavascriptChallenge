dc4183a7d4ed23b162d55b45956ad6010f413bf4
Gustavo <gar.santeliz@gmail.com>
2021-11-23 12:42:04 -0500

    trying to solve problem when array is [0,1,0]

--- src/component/MoveZeroes.js
+++ src/component/MoveZeroes.js
@@ -3,15 +3,28 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
  var moveZeroes = function(nums) {
+    
     for (let i = 0; i < nums.length; i++){
         console.log(nums[i])
         
-     if (nums[i] === 0){
+        if (nums[i] < 1){
             nums.shift(nums[i])
-            nums.push(nums[i])
+            console.log(nums[i])
+            
+            nums.push(0)
+        }
+        else if(nums[0] === 0){
+            return nums[0] = 1;
             console.log(nums)
         }
+        else if (nums.length < 2){
+            return nums=[0]
+            console.log(nums)
+        }
+                         
+    
     }
     return nums
-    console.log(nums)
 };
+
+export default moveZeroes;

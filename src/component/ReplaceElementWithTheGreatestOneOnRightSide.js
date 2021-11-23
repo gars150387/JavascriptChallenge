aeca0532db19386ea37bfa487724ae83208de646
Gustavo <gar.santeliz@gmail.com>
2021-11-23 11:12:26 -0500

    when arr contains one element must return -1

--- src/component/ReplaceElementWithTheGreatestOneOnRightSide.js
+++ src/component/ReplaceElementWithTheGreatestOneOnRightSide.js
@@ -2,27 +2,29 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-
-// result is wrong answer because when it is test with an array = [400]
-//shoulb be return -1 
-
-
  var replaceElements = function(arr) {
-    arr[0] = Math.max(...arr)
-    // console.log(arr[0])
-    arr[arr.length] = -1
-    // console.log(arr.length)
+    arr[0] = Math.max(...arr);
+    console.log(arr)
     
-   for ( let i = arr.length - 1; i > 0; i--){
-       // console.log(arr[i])
+   for ( let i = arr.length; i > 0; i--){
+       // if(arr[i.length] == undefined){
+       //     arr.push(-1)
+       // }
+    
        if (arr[i] > arr[i-1]){
            arr[i-1] = arr[i]
-           // console.log(arr[i-1])
+           console.log(arr)
+           
        }
        else if (arr[0] === arr[1]){
-           arr.slice(1)
+           arr.shift()
+           arr.push(-1)
+           console.log(arr)
        }
+       else if( arr.length < 2){
+           return arr = [-1]
+       }
+       
    }
-   return arr
-
-}
+   return arr;
+};
